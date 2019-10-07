b73a2a1f799a6a952120977f15cc7469f991c01e
Łukasz Kania <waniaskfr@gmail.com>
2019-10-07 13:22:49 +0200

    change script.js add results

--- js/script.js
+++ js/script.js
@@ -31,3 +31,34 @@ if(playerInput == '1'){
 
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+  if( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Przegrywasz!');
+  }
+  if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+  }
+  if( computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+  }
+  if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Przegrywasz!');
+  }
+  if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  }
+  if( computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Przegrywasz!');
+  }
+  if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  }
+  if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+  }
+  if( playerMove == 'nieznany ruch'){
+      printMessage('wybierz właściwą liczbę')
+  }
